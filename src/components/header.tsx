5d8cea74e296bff1266b5d4b6f46e6a03208d02c
jsnicholas <jsnicholas@pm.me>
2023-06-07 14:00:30 -0400

    add mobile menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ function Header() {
       <div className="flex-1">
         <a className="btn btn-ghost normal-case text-xl">✌️ nick johnson</a>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
+      <div className="">
+        <ul className="max-md:invisible menu menu-horizontal px-1">
           <li>
             <a href="#about">About</a>
           </li>
@@ -17,6 +17,27 @@ function Header() {
           </li>
           <li>
             <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        <ul className="md:invisible md:max-w-0">
+          <li>
+            <details className="dropdown dropdown-end">
+              <summary className="m-1 btn">menu</summary>
+              <ul className="p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52">
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#projects">Code</a>
+                </li>
+                <li>
+                  <a href="#creative">Creative</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </details>
           </li>
         </ul>
       </div>
