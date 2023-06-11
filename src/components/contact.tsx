a82c3b9962fa042a2d3bab7962b8bce5b898ee32
jsnicholas <jsnicholas@pm.me>
2023-06-11 09:50:33 -0400

    update contact section

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -5,35 +5,22 @@ function Contact() {
       <p className="my-4 text-lg font-semibold">
         Whether it's through code, brand identity development, or curriculum
         building, I'd love to help add to your company's story.{" "}
-        <span className="gradientText">Let's build something together.</span>
+        <span className="gradientText">Let's build something together.</span>{" "}
+        Say hello: <br />
+        <ul className="my-2">
+          <li>
+            <a href="mailto:jsnicholas@pm.me">→ jsnicholas@pm.me</a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/jsnicholas">→ LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/jsnicholas">→ GitHub</a>
+          </li>
+        </ul>
       </p>
-      <div className="container content-center mx-auto my-8">
-        <form className="join w-full">
-          <input
-            id="messageContent"
-            name="message"
-            type="textarea"
-            placeholder="Your Message"
-            className="join-item textarea textarea-bordered textarea-md w-full"
-          />
-          <input
-            value="Send"
-            type="button"
-            className="join-item max-w-lg btn btn-lg btn-outline btn-secondary"
-            onClick={(event) => {
-              handleMail(event);
-            }}
-          />
-        </form>
-      </div>
     </section>
   );
-}
-const messageContent = document.getElementById("messageContent");
-function handleMail(event) {
-  event.preventDefault;
-  //@ts-expect-error: value will be null until user creates input
-  window.location.href = `mailto:jsnicholas@pm.me?body=${messageContent.value}`;
 }
 
 export default Contact;
